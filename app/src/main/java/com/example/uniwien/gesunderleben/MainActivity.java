ba51be2a61539d99f935039fd85f0faf3ed2535a
Ornetsmüller Raphael <a1425348@unet.univie.ac.at>
2017-05-16 12:29:15 +0200

    Pläne_angebunden_an_Hauptmenü

--- app/src/main/java/com/example/uniwien/gesunderleben/MainActivity.java
+++ app/src/main/java/com/example/uniwien/gesunderleben/MainActivity.java
@@ -2,18 +2,8 @@ package com.example.uniwien.gesunderleben;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -30,8 +20,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.buttonDailyPlan: {
-                //Intent intent = new Intent(this, UserData4.class);
-                //startActivity(intent);
+                Intent intent = new Intent(this, Planner.class);
+                startActivity(intent);
                 break;
             }
             case R.id.buttonProgress: {
