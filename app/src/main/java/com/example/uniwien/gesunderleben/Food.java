5c36b161c8e834e859e441fd156078ce4b641211
Max Behrens <maxi.behrens@hotmail.com>
2017-05-16 08:34:12 +0000

    Upload new file

---
+++ app/src/main/java/com/example/uniwien/gesunderleben/Food.java
@@ -0,0 +1,21 @@
+package com.example.uniwien.gesunderleben;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class Food extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_food, container, false);
+        return rootView;
+    }
+}
